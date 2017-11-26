852dcce314e8c4d375d79da20b5f3e0a81da20b1
Dejan Trajkovic <dtrajko@gmail.com>
2017-11-26 05:20:45 +0100

    Styling and UX improvements of the modal popup take 2

--- app/components/flash_card/FlashCard_css.js
+++ app/components/flash_card/FlashCard_css.js
@@ -16,6 +16,7 @@ const styles = StyleSheet.create({
     section_score_text: {
         fontSize: 24,
         fontWeight: 'bold',
+        marginBottom: 10,
     },
     section_picture: {
         flex: 8,
@@ -67,13 +68,16 @@ const styles = StyleSheet.create({
     },
     modal_popup: {
         flex: 1,
+        marginTop: 90,
+        marginBottom: 20,
         padding: 10,
-        paddingBottom: 90,
+        paddingBottom: 70,
         backgroundColor: '#FFF',
         justifyContent: 'flex-end',
         alignSelf: 'stretch',
         alignItems: 'center',
         opacity: 1,
+        borderRadius: 20,
     },
     modal_popup_correct: {
         // backgroundColor: '#e6ffe6',
